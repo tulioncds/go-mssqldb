bcd9e1ce25110eb7b4681b51433d016e56c5ddbf
davidshi <david.shiflet@microsoft.com>
2023-06-14 15:22:13 -0500

    build fixes

--- aecmk/localcert/keyprovider_darwin.go
+++ aecmk/localcert/keyprovider_darwin.go
@@ -1,5 +1,11 @@
 package localcert
 
+import (
+	"crypto/x509"
+	"fmt"
+)
+
 func (p *LocalCertProvider) loadWindowsCertStoreCertificate(path string) (privateKey interface{}, cert *x509.Certificate) {
 	panic(fmt.Errorf("Windows cert store not supported on this OS"))
+	return
 }
