c53676e1151cc93e05d7b00cdd4304d290898839
davidshi <david.shiflet@microsoft.com>
2023-07-25 09:09:09 -0500

    fix unit tests

--- internal/github.com/swisscom/mssql-always-encrypted/pkg/alwaysencrypted_test.go
+++ internal/github.com/swisscom/mssql-always-encrypted/pkg/alwaysencrypted_test.go
@@ -20,125 +20,87 @@ import (
 
 func TestLoadCEKV(t *testing.T) {
 	certFile, err := os.Open("../test/always-encrypted_pub.pem")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	certBytes, err := ioutil.ReadAll(certFile)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	pemB, _ := pem.Decode(certBytes)
 	cert, err := x509.ParseCertificate(pemB.Bytes)
-	if err != nil {
-		t.Fatal(nil)
-	}
+	assert.NoError(t, err)
 
 	cekvFile, err := os.Open("../test/cekv.key")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	cekvBytes, err := ioutil.ReadAll(cekvFile)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	cekv := LoadCEKV(cekvBytes)
 	assert.Equal(t, 1, cekv.Version)
 	assert.True(t, cekv.Verify(cert))
 }
 func TestDecrypt(t *testing.T) {
 	certFile, err := os.Open("../test/always-encrypted.pem")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	certBytes, err := ioutil.ReadAll(certFile)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	pemB, _ := pem.Decode(certBytes)
 	privKey, err := x509.ParsePKCS8PrivateKey(pemB.Bytes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	rsaPrivKey := privKey.(*rsa.PrivateKey)
 
 	cekvFile, err := os.Open("../test/cekv.key")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	cekvBytes, err := ioutil.ReadAll(cekvFile)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	cekv := LoadCEKV(cekvBytes)
 	rootKey, err := cekv.Decrypt(rsaPrivKey)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	assert.Equal(t, "0ff9e45335df3dec7be0649f741e6ea870e9d49d16fe4be7437ce22489f48ead", fmt.Sprintf("%02x", rootKey))
 	assert.Equal(t, 1, cekv.Version)
 	assert.NotNil(t, rootKey)
 
 	columnBytesFile, err := os.Open("../test/column_value.enc")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	columnBytes, err := ioutil.ReadAll(columnBytesFile)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	key := keys.NewAeadAes256CbcHmac256(rootKey)
 	alg := algorithms.NewAeadAes256CbcHmac256Algorithm(key, encryption.Deterministic, 1)
 	cleartext, err := alg.Decrypt(columnBytes)
-	if err != nil {
-		t.Fatalf("Decrypt failed! %s", err.Error())
-	}
+	assert.NoErrorf(t, err, "Decrypt failed! %v", err)
+
 	enc := unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM)
 	decoder := enc.NewDecoder()
 	cleartextUtf8, err := decoder.Bytes(cleartext)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	t.Logf("column value: \"%02X\"", cleartextUtf8)
 	assert.Equal(t, "12345     ", string(cleartextUtf8))
 }
 func TestDecryptCEK(t *testing.T) {
 	certFile, err := os.Open("../test/always-encrypted.pem")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	certFileBytes, err := ioutil.ReadAll(certFile)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	pemBlock, _ := pem.Decode(certFileBytes)
 	cert, err := x509.ParsePKCS8PrivateKey(pemBlock.Bytes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	cekvFile, err := os.Open("../test/cekv.key")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	cekvBytes, err := ioutil.ReadAll(cekvFile)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
 	cekv := LoadCEKV(cekvBytes)
-	fmt.Printf("Cert: %v\n", cert)
+	t.Logf("Cert: %v\n", cert)
 
 	rsaKey := cert.(*rsa.PrivateKey)
 
 	// RSA/ECB/OAEPWithSHA-1AndMGF1Padding
 	bytes, err := rsa.DecryptOAEP(sha1.New(), rand.Reader, rsaKey, cekv.Ciphertext, nil)
-	fmt.Printf("Key: %02x\n", bytes)
+	assert.NoError(t, err)
+	t.Logf("Key: %02x\n", bytes)
 }
